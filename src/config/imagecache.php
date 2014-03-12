3dd76b2f5bf43296c6b4ff14b87e1123e44bd786
Oliver Vogel <oliver@olivervogel.net>
2014-03-12 17:53:26 +0100

    added configuration file

---
+++ src/config/imagecache.php
@@ -0,0 +1,76 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Name of route
+    |--------------------------------------------------------------------------
+    |
+    | Enter the routes name to enable dynamic imagecache manipulation.
+    | This handle will define the first part of the URI:
+    | 
+    | {route}/{template}/{filename}
+    | 
+    | Examples: "images", "img/cache"
+    |
+    */
+   
+    'route' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Storage paths
+    |--------------------------------------------------------------------------
+    |
+    | The following paths will be searched for the image filename, submited 
+    | by URI. 
+    | 
+    | Define as mich directories as you like.
+    |
+    */
+    
+    'paths' => array(
+        public_path('upload'),
+        public_path('images')
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Manipulation templates
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your own manipulation callbacks.
+    | The keys of this array will define which templates 
+    | are available in the URI:
+    |
+    | {route}/{template}/{filename}
+    |
+    */
+   
+    'templates' => array(
+
+        'small' => function($image) { 
+            return $image->grab(120, 90);
+        },
+        'medium' => function($image) {
+            return $image->grab(240, 180);
+        },
+        'large' => function($image) {
+            return $image->grab(480, 360);
+        }
+
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Image Cache Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Lifetime in minutes of the images handled by the imagecache route.
+    |
+    */
+   
+    'lifetime' => 43200,
+
+);
